39a0de78832e11cb79dbbe6e3c4f74992436cbd8
SS-Stefanov1 <137191949+SS-Stefanov1@users.noreply.github.com>
2024-02-21 11:46:57 +0200

    v0.01
    - Adding more planets
    - Organizing the code into groups
    - Removing unused code
    - Optimizing the CSS

--- main.js
+++ main.js
@@ -2,82 +2,126 @@ import './style.css';
 import * as Three from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+
+// Load Textures
+const cat_texture     = new Three.TextureLoader().load('/images/cat.jpg');
+const sun_texture     = new Three.TextureLoader().load('/images/sun.jpg');
+const mercury_texture = new Three.TextureLoader().load('/images/mercury.jpg');
+const venus_texture   = new Three.TextureLoader().load('/images/venus.jpg');
+const earth_texture   = new Three.TextureLoader().load('/images/earth.jpg');
+const mars_texture    = new Three.TextureLoader().load('/images/mars.jpg');
+const spaceT          = new Three.TextureLoader().load('/images/space_bg.jpg');
+// -----
+
+
 // Initiate Scene
-const scene  = new Three.Scene();
-const camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
-const render = new Three.WebGL1Renderer({ canvas : document.querySelector('#bg') });
+const scene   = new Three.Scene();
+const camera  = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
+const render  = new Three.WebGL1Renderer({ canvas : document.querySelector('#bg') });
+const control = new OrbitControls(camera,render.domElement);
 
 render.setPixelRatio( window.devicePixelRatio );
 render.setSize( window.innerWidth, window.innerHeight );
-camera.position.z = 30;
-camera.position.x = -3;
+
+camera.position.z = 0;
+camera.position.y = 0;
+camera.position.x = 0;
+
+scene.background = spaceT;
+// -----
 
 
-const shape  = new Three.SphereGeometry( 16, 64, 16 ); 
-const shader = new Three.MeshStandardMaterial( { color: 0xFF6347} );
-const flyer  = new Three.Mesh( shape, shader );
+// Lighting
 const plight = new Three.PointLight(0xFFFFFF);
 const alight = new Three.AmbientLight(0xFFFFFF);
-const contrl = new OrbitControls(camera,render.domElement);
-const cat_tx = new Three.TextureLoader().load('images/cat.jpg');
-const cat    = new Three.Mesh(
-    new Three.BoxGeometry(3,3,3),
-    new Three.MeshBasicMaterial( { map: cat_tx } ),
-);
-
-
-
 
 plight.position.set( 15,0,0 );
 alight.position.set( 15,0,0 );
 
+scene.add(plight,alight);
+// -----
 
-scene.add(plight,alight,flyer);
+
+// Scene Objects
+const sun  = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: sun_texture } ),
+);
+
+const mercury  = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: mercury_texture } ),
+);
+
+const venus  = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: venus_texture } ),
+);
+
+const earth  = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: earth_texture } ),
+);
+
+const mars  = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: mars_texture } ),
+);
+
+const cat    = new Three.Mesh(
+    new Three.BoxGeometry(3,3,3),
+    new Three.MeshBasicMaterial( { map: cat_texture } ),
+);
+
+// Scene Objects Coordinates 
+mars.position.x = -50;
+mars.position.y = 50;
+mars.position.z = 350;
+
+scene.add(earth,mars);
+// -----
 
 
+
+
+// Functions
 function addStars() {
-    const s_ge = new Three.SphereGeometry(0.05, 5, 5);
+    const s_ge = new Three.SphereGeometry(0.5, 5, 5);
     const s_mt = new Three.MeshStandardMaterial( { color: 0xF0F0F0 } );
     const star = new Three.Mesh( s_ge, s_mt );
 
-    const [x,y,z] = Array(3).fill().map(() => Three.MathUtils.randFloatSpread( 100 ) );
+    const [x,y,z] = Array(3).fill().map(() => Three.MathUtils.randFloatSpread( 1200 ) );
     star.position.set(x,y,z);
     scene.add(star);
 }
+Array(2000).fill().forEach(addStars);
 
-Array(800).fill().forEach(addStars);
-const spaceT = new Three.TextureLoader().load('images/space_bg.jpg');
-spaceT.wrapS = Three.RepeatWrapping;
-spaceT.wrapT = Three.RepeatWrapping;
-scene.background = spaceT;
 
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
 
-    camera.position.x = t * -0.0002;
-    camera.position.y = t * -0.0002;
-    camera.position.z = t * -0.1;
+    // camera.position.x = t * -0.5;
+    // camera.position.y = t * -0.5;
+    camera.position.z = t * -0.5;
 
-    flyer.position.x += 0.002;
-    flyer.position.y -= 0.005;
+    // earth.position.x += 0.002;
+    // earth.position.y -= 0.005;
+
+    // mars.position.x -= 0.5;
+    // mars.position.y += 0.5;
 }
-
-// window.onscroll = function(e) {
-//     console.log(this.oldScroll > this.scroolY);
-//     this.oldScroll = this.scroolY;
-// }
-
 document.body.onscroll = moveCamera;
+moveCamera();
+
 
 function animate() {
     requestAnimationFrame( animate );
 
-    flyer.rotation.x += 0.01;
-    flyer.rotation.y += 0.20000000;
-    flyer.rotation.z += 0.01;
+    earth.rotation.y += 0.002;
+    mars.rotation.y  += 0.002;
 
-    contrl.update();
-
+    control.update();
     render.render( scene, camera );
 }
 animate();
+// -----
