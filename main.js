a58a925b1fad3047fa2bedce2c0fe064f59a92e1
SS-Stefanov1 <137191949+SS-Stefanov1@users.noreply.github.com>
2024-02-21 14:21:38 +0200

    v0.03
    - Organized images
    - Added more objects
    - Added different animations
    - Tweaks

--- main.js
+++ main.js
@@ -5,25 +5,32 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Load Textures
 const cat_texture     = new Three.TextureLoader().load('/images/cat.jpg');
-const sun_texture     = new Three.TextureLoader().load('/images/sun.jpg');
-const mercury_texture = new Three.TextureLoader().load('/images/mercury.jpg');
-const venus_texture   = new Three.TextureLoader().load('/images/venus.jpg');
-const earth_texture   = new Three.TextureLoader().load('/images/earth.jpg');
-const mars_texture    = new Three.TextureLoader().load('/images/mars.jpg');
 const spaceT          = new Three.TextureLoader().load('/images/space_bg.jpg');
+const sun_texture     = new Three.TextureLoader().load('/images/_planets/sun.jpg');
+const mercury_texture = new Three.TextureLoader().load('/images/_planets/mercury.jpg');
+const venus_texture   = new Three.TextureLoader().load('/images/_planets/venus.jpg');
+const earth_texture   = new Three.TextureLoader().load('/images/_planets/earth.jpg');
+const mars_texture    = new Three.TextureLoader().load('/images/_planets/mars.jpg');
+const jupiter_texture = new Three.TextureLoader().load('/images/_planets/jupiter.jpg');
+const saturn_texture  = new Three.TextureLoader().load('/images/_planets/saturn.jpg');
+const uranus_texture  = new Three.TextureLoader().load('/images/_planets/uranus.jpg');
+const neptune_texture = new Three.TextureLoader().load('/images/_planets/neptune.jpg');
 // -----
 
 
 // Initiate Scene
 const scene   = new Three.Scene();
-const camera  = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
+const camera  = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 ); 
 const render  = new Three.WebGL1Renderer({ canvas : document.querySelector('#bg') });
 const control = new OrbitControls(camera,render.domElement);
 
 render.setPixelRatio( window.devicePixelRatio );
 render.setSize( window.innerWidth, window.innerHeight );
 
-camera.position.z = 0;
+// const grid = new Three.GridHelper();
+// scene.add(grid);
+
+camera.position.z = -20;
 camera.position.y = 0;
 camera.position.x = 0;
 
@@ -32,13 +39,14 @@ scene.background = spaceT;
 
 
 // Lighting
-const plight = new Three.PointLight(0xFFFFFF);
-const alight = new Three.AmbientLight(0xFFFFFF);
+const ambient_light = new Three.AmbientLight(0xFFFFFF);
+const point_light = new Three.PointLight(0xFFFFFF, 5000);
+const point_light_helper = new Three.PointLightHelper(point_light,1);
 
-plight.position.set( 15,0,0 );
-alight.position.set( 15,0,0 );
+point_light.position.set( 450,150,-900 );
+ambient_light.position.set( 15,0,0 );
 
-scene.add(plight,alight);
+scene.add(point_light,point_light_helper,ambient_light);
 // -----
 
 
@@ -47,6 +55,7 @@ const sun = new Three.Mesh(
     new Three.SphereGeometry( 256, 16, 16 ),
     new Three.MeshBasicMaterial( { map: sun_texture } ),
 );
+
 
 const mercury = new Three.Mesh( 
     new Three.SphereGeometry( 16, 64, 16 ),
@@ -68,43 +77,92 @@ const mars = new Three.Mesh(
     new Three.MeshBasicMaterial( { map: mars_texture } ),
 );
 
+const jupiter = new Three.Mesh( 
+    new Three.SphereGeometry( 90, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: jupiter_texture } ),
+);
+
+const saturn = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: saturn_texture } ),
+);
+
+const uranus = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: uranus_texture } ),
+);
+
+const neptune = new Three.Mesh( 
+    new Three.SphereGeometry( 16, 64, 16 ),
+    new Three.MeshBasicMaterial( { map: neptune_texture } ),
+);
+
 const cat = new Three.Mesh(
     new Three.BoxGeometry(3,3,3),
     new Three.MeshBasicMaterial( { map: cat_texture } ),
 );
 
 // Scene Object(s) Positions 
-
-sun.position.x = 250;
+sun.position.x = 450;
 sun.position.y = 150;
-sun.position.z = -950;
+sun.position.z = -900;
 
-mercury.position.x = 75;
-mercury.position.y = 55;
-mercury.position.z = -700;
+mercury.position.x = 225;
+mercury.position.y = 25;
+mercury.position.z = -750;
 
-venus.position.x = -250;
-venus.position.y = 50;
-venus.position.z = -350;
+venus.position.x = -120;
+venus.position.y = 75;
+venus.position.z = -250;
 
 earth.position.x = -10;
 earth.position.y = -10;
-earth.position.z = 0;
+earth.position.z = 150;
 
 mars.position.x = -50;
 mars.position.y = 50;
-mars.position.z = 350;
+mars.position.z = 450;
+
+jupiter.position.x = 150;
+jupiter.position.y = -80;
+jupiter.position.z = 850;
+
+saturn.position.x = -250;
+saturn.position.y = -150;
+saturn.position.z = 1250;
+
+uranus.position.x = 50;
+uranus.position.y = 50;
+uranus.position.z = 1550;
+
+neptune.position.x = -150;
+neptune.position.y = 50;
+neptune.position.z = 1950;
 
 
-scene.add(sun,mercury,venus,earth,mars);
+scene.add(sun,mercury,venus,earth,saturn,uranus,neptune,mars,jupiter);
 // -----
 
 
+// Animation 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const fadeElements = document.querySelectorAll('.dummy,.center_it');
+fadeElements.forEach((el) => observer.observe(el));
+// -----
 
 
 // Functions
 function addStars() {
-    const s_ge = new Three.SphereGeometry(0.5, 5, 5);
+    const s_ge = new Three.SphereGeometry(0.2, 5, 5);
     const s_mt = new Three.MeshStandardMaterial( { color: 0xF0F0F0 } );
     const star = new Three.Mesh( s_ge, s_mt );
 
@@ -135,9 +193,9 @@ moveCamera();
 function animate() {
     requestAnimationFrame( animate );
 
-    sun.rotation.y      += 0.002;
+    sun.rotation.y      += 0.0002;
     mercury.rotation.y  += 0.002;
-    venus.rotation.y    += 0.002;
+    venus.rotation.y    += 0.0002;
     earth.rotation.y    += 0.002;
     mars.rotation.y     += 0.002;
 
